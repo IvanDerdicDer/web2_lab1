69776f92572d5ad6ca5083192f195631370c6325
IvanDerdicDer <46329317+IvanDerdicDer@users.noreply.github.com>
2022-10-25 19:50:56 +0200

    REa

--- app.py
+++ app.py
@@ -61,9 +61,4 @@ def logout():
 
 
 if __name__ == '__main__':
-    external_url = env.get("RENDER_EXTERNAL_URL")
-    external_port = env.get('PORT')
-    if external_url and external_port:
-        app.run(host=external_url, port=external_port)
-    else:
-        app.run()
+    app.run()
