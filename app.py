25cad8f999a60887379917cce74b2c22bc654d52
IvanDerdicDer <46329317+IvanDerdicDer@users.noreply.github.com>
2022-10-28 19:42:15 +0200

    FEATURES

--- app.py
+++ app.py
@@ -1,15 +1,20 @@
 import json
+from enum import Enum, auto
+from functools import wraps
 from os import environ as env
 from urllib.parse import quote_plus, urlencode
+from itertools import count
+from typing import Generator
+
 from authlib.integrations.flask_client import OAuth
 from dotenv import find_dotenv, load_dotenv
-from flask import Flask, redirect, render_template, session, url_for, jsonify
+from flask import Flask, redirect, render_template, session, url_for, jsonify, request
+
 from project_data import *
-from functools import wraps
 
-users, teams, games = get_all_data()
+users_dict, teams_dict, games_dict, comments_dict = get_all_data()
 
-groups = calculate_points(games, teams)
+groups_dict = calculate_points(games_dict, teams_dict)
 
 ENV_FILE = find_dotenv()
 if ENV_FILE:
@@ -33,8 +38,29 @@ oauth.register(
 )
 
 
-def authenticate(is_admin: Optional[bool] = None) -> bool:
-    if is_admin is None:
+def game_id_generator(x: int) -> Generator:
+    for i in count(x):
+        yield str(i)
+
+
+def comment_id_generator(x: int = 1) -> Generator:
+    for i in count(x):
+        yield str(i)
+
+
+game_id_counter = game_id_generator(int(max(games_dict.keys())) + 1)
+comment_id_counter = comment_id_generator()
+
+
+class AuthSelector(Enum):
+    NO_AUTH = auto()
+    USER = auto()
+    ADMIN = auto()
+    USER_AND_ADMIN = auto()
+
+
+def authenticate(auth_selector: AuthSelector = AuthSelector.NO_AUTH) -> bool:
+    if auth_selector == AuthSelector.NO_AUTH:
         return True
 
     if 'user' not in session:
@@ -42,20 +68,26 @@ def authenticate(is_admin: Optional[bool] = None) -> bool:
 
     user_id = session['user']['userinfo']['sub']
 
-    if user_id in users and is_admin == False and users[user_id] == False:
-        return True
+    if auth_selector == AuthSelector.USER:
+        if user_id in users_dict and not users_dict[user_id]:
+            return True
 
-    if user_id in users and is_admin == True and users[user_id] == True:
-        return True
+    if auth_selector == AuthSelector.ADMIN:
+        if user_id in users_dict and users_dict[user_id]:
+            return True
+
+    if auth_selector == AuthSelector.USER_AND_ADMIN:
+        if user_id in users_dict:
+            return True
 
     return False
 
 
-def endpoint_auth(is_admin: Optional[bool] = None):
+def endpoint_auth(auth_selector: AuthSelector = AuthSelector.NO_AUTH):
     def decorate(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
-            is_auth = authenticate(is_admin)
+            is_auth = authenticate(auth_selector)
             if not is_auth:
                 return jsonify({'message': 'Unauthorised'}), 401
 
@@ -68,7 +100,120 @@ def endpoint_auth(is_admin: Optional[bool] = None):
 
 @app.route("/")
 def home():
-    return render_template("home.html", session=session.get('user'), pretty=json.dumps(session.get('user'), indent=4))
+    return render_template("home.html", session=session.get('user'))
+
+
+@app.route('/groups')
+@endpoint_auth(AuthSelector.NO_AUTH)
+def groups():
+    return render_template('groups.html', groups=groups_dict)
+
+
+@app.route('/games')
+@endpoint_auth(AuthSelector.NO_AUTH)
+def games():
+    return render_template('games.html', games=games_dict, is_admin=authenticate(AuthSelector.ADMIN))
+
+
+@app.route('/edit_game/<game_id>')
+@endpoint_auth(AuthSelector.ADMIN)
+def edit_game(game_id):
+    if game_id not in games_dict:
+        return 'Game not found'
+
+    return render_template('edit_game.html', game=games_dict[game_id], game_id=game_id)
+
+
+@app.route('/create_game', methods=['GET'])
+@endpoint_auth(AuthSelector.ADMIN)
+def create_game():
+    return render_template('create_game.html')
+
+
+@app.route('/create_game_api', methods=['POST'])
+@endpoint_auth(AuthSelector.ADMIN)
+def create_game_api():
+    global groups_dict
+
+    game: Dict[str, Any] = request.form.to_dict()
+    game['team_a_score'] = int(game['team_a_score'])
+    game['team_b_score'] = int(game['team_b_score'])
+
+    if game['team_a'] not in teams_dict:
+        return f"Team {game['team_a']} does not exist."
+
+    if game['team_b'] not in teams_dict:
+        return f"Team {game['team_b']} does not exist."
+
+    game_id = next(game_id_counter)
+
+    if game_id in games_dict:
+        return 'Game exists.'
+
+    games_dict[game_id] = game
+
+    print(json.dumps(games_dict, ensure_ascii=False, indent=4))
+
+    groups_dict = calculate_points(games_dict, teams_dict)
+
+    return render_template('games.html', games=games_dict, is_admin=authenticate(AuthSelector.ADMIN))
+
+
+@app.route('/update_game/<game_id>', methods=['POST'])
+@endpoint_auth(AuthSelector.ADMIN)
+def update_game(game_id):
+    global groups_dict
+
+    game: Dict[str, Any] = request.form.to_dict()
+    game['team_a_score'] = int(game['team_a_score'])
+    game['team_b_score'] = int(game['team_b_score'])
+
+    if game_id not in games_dict:
+        return 'Unable to find game.'
+
+    games_dict[game_id] = game
+
+    groups_dict = calculate_points(games_dict, teams_dict)
+
+    return render_template('games.html', games=games_dict, is_admin=authenticate(AuthSelector.ADMIN))
+
+
+@app.route('/delete_game/<game_id>')
+@endpoint_auth(AuthSelector.ADMIN)
+def delete_game(game_id):
+    global groups_dict
+
+    if game_id not in games_dict:
+        return 'Game not found'
+
+    games_dict.pop(game_id)
+
+    if game_id in comments_dict:
+        comments_dict.pop(game_id)
+
+    groups_dict = calculate_points(games_dict, teams_dict)
+
+    return render_template('games.html', games=games_dict, is_admin=authenticate(AuthSelector.ADMIN))
+
+
+@app.route('/comments/<game_id>')
+@endpoint_auth(AuthSelector.NO_AUTH)
+def comments(game_id):
+    if game_id not in comments_dict:
+        return 'Comments not found'
+
+    user_id = None
+    if 'user' in session:
+        user_id = session['user']['userinfo']['sub']
+
+    return render_template(
+        'comments.html',
+        comments=comments_dict[game_id],
+        game_id=game_id,
+        is_user=authenticate(AuthSelector.USER),
+        is_admin=authenticate(AuthSelector.ADMIN),
+        user_id=user_id
+    )
 
 
 @app.route("/login")
--- project_data.py
+++ project_data.py
@@ -1,18 +1,7 @@
-from dataclasses import dataclass, field
-from dataclasses_json import dataclass_json
-from typing import List, Tuple, Dict, Optional
+from typing import Tuple, Dict, Any
 
 
-@dataclass_json()
-@dataclass()
-class Game:
-    team_a: str
-    team_b: str
-    team_a_score: int = field(default=0)
-    team_b_score: int = field(default=0)
-
-
-def get_all_data() -> Tuple[Dict[str, bool], Dict[str, str], Dict[str, Game]]:
+def get_all_data() -> Tuple[Dict[str, bool], Dict[str, str], Dict[str, Dict], Dict[str, Dict[str, Any]]]:
     users: Dict[str, bool] = {
         'auth0|6356ed1d5615e6a1bdb56435': False,
         'auth0|6356ed948d3ef662e0286c6d': False,
@@ -32,34 +21,36 @@ def get_all_data() -> Tuple[Dict[str, bool], Dict[str, str], Dict[str, Game]]:
         'Varaždin': 'A'
     }
 
-    games: Dict[str, Game] = {
-        '1': Game(
-            'Dinamo',
-            'Hajduk',
-            1,
-            0
+    games: Dict[str, dict] = {
+        '1': dict(
+            team_a='Dinamo',
+            team_b='Hajduk',
+            team_a_score=1,
+            team_b_score=0
         ),
-        '2': Game(
-            'Slaven Belupo',
-            'Hajduk',
-            1,
-            2
+        '2': dict(
+            team_a='Slaven Belupo',
+            team_b='Hajduk',
+            team_a_score=1,
+            team_b_score=2
         ),
-        '3': Game(
-            'Dinamo',
-            'Šibenik',
-            3,
-            1
+        '3': dict(
+            team_a='Dinamo',
+            team_b='Šibenik',
+            team_a_score=3,
+            team_b_score=1
         ),
-        '4': Game(
-            'Varaždin',
-            'Šibenik',
-            1,
-            1
+        '4': dict(
+            team_a='Varaždin',
+            team_b='Šibenik',
+            team_a_score=1,
+            team_b_score=1
         )
     }
 
-    return users, teams, games
+    comments = {i: dict() for i in games}
+
+    return users, teams, games, comments
 
 
 def relu(x):
@@ -69,27 +60,27 @@ def relu(x):
 
 
 def calculate_points(
-        games: Dict[str, Game],
+        games: Dict[str, dict],
         teams: Dict[str, str]
 ) -> Dict[str, Dict[str, int]]:
     groups: Dict[str, Dict[str, int]] = {}
 
     for game in games.values():
-        if teams[game.team_a] not in groups:
-            groups[teams[game.team_a]] = {}
+        if teams[game['team_a']] not in groups:
+            groups[teams[game['team_a']]] = {}
 
-        if game.team_a not in groups[teams[game.team_a]]:
-            groups[teams[game.team_a]][game.team_a] = 0
+        if game['team_a'] not in groups[teams[game['team_a']]]:
+            groups[teams[game['team_a']]][game['team_a']] = 0
 
-        if game.team_b not in groups[teams[game.team_a]]:
-            groups[teams[game.team_a]][game.team_b] = 0
+        if game['team_b'] not in groups[teams[game['team_a']]]:
+            groups[teams[game['team_a']]][game['team_b']] = 0
 
-        if game.team_a_score == game.team_b_score:
-            groups[teams[game.team_a]][game.team_a] += 1
-            groups[teams[game.team_a]][game.team_b] += 1
+        if game['team_a_score'] == game['team_b_score']:
+            groups[teams[game['team_a']]][game['team_a']] += 1
+            groups[teams[game['team_a']]][game['team_b']] += 1
             continue
 
-        groups[teams[game.team_a]][game.team_a] += relu(game.team_a_score - game.team_b_score)
-        groups[teams[game.team_a]][game.team_b] += relu(game.team_b_score - game.team_a_score)
+        groups[teams[game['team_a']]][game['team_a']] += relu(game['team_a_score'] - game['team_b_score'])
+        groups[teams[game['team_a']]][game['team_b']] += relu(game['team_b_score'] - game['team_a_score'])
 
     return groups
